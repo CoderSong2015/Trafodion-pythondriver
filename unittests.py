4b82b1e54241a96f73c22e674e1531466dfe2d95
SuJinpei <873118042@qq.com>
2018-07-13 11:41:20 +0800

    add module interface test case

--- unittests.py
+++ unittests.py
@@ -1,8 +1,10 @@
 import unittest
 from tests import test_datatype
+from tests import test_module_interface
 
 if __name__ == '__main__':
     su = unittest.TestSuite()
-    su.addTests(unittest.makeSuite(test_datatype.TestsCursor))
-    runner = unittest.TextTestRunner()
+    # su.addTests(unittest.makeSuite(test_datatype.TestsCursor))
+    su.addTests(unittest.makeSuite(test_module_interface.TestModuleInterface))
+    runner = unittest.TextTestRunner(verbosity=2)
     runner.run(su)
