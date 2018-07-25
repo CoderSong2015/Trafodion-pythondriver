0e19a6ec236403e364a7a26906ee4e6c5eef8e40
SuJinpei <873118042@qq.com>
2018-07-25 10:25:44 +0800

    add cases

--- unittests.py
+++ unittests.py
@@ -1,10 +1,16 @@
 import unittest
 from tests import test_datatype
 from tests import test_module_interface
+from tests import test_connection_object
+from tests import test_cursor_objects
+from tests import test_type_objects
 
 if __name__ == '__main__':
     su = unittest.TestSuite()
     # su.addTests(unittest.makeSuite(test_datatype.TestsCursor))
-    su.addTests(unittest.makeSuite(test_module_interface.TestModuleInterface))
+    # su.addTests(unittest.makeSuite(test_module_interface.TestModuleInterface))
+    # su.addTest(unittest.makeSuite(test_connection_object.TestConnectionObject))
+    # su.addTest(unittest.makeSuite(test_cursor_objects.TestCursorObject))
+    su.addTest(unittest.makeSuite(test_type_objects.TestTypeObject))
     runner = unittest.TextTestRunner(verbosity=2)
     runner.run(su)
