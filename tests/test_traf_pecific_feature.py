a9523f723ec29be0564a44db3383d8bc09203e61
SuJinpei <873118042@qq.com>
2018-08-17 10:02:23 +0800

    add test case for trafodion specific feature

---
+++ tests/test_traf_pecific_feature.py
@@ -0,0 +1,23 @@
+import unittest
+from pdbc.trafodion import connector
+
+class TestTrafFeature(unittest.TestCase):
+    @unittest.skip('need manually add to config.py')
+    def test_multi_tenant(self):
+        pass
+
+    @unittest.skip("unsupported by python driver")
+    def test_multi_IP(self):
+        pass
+
+    @unittest.skip("unsupported by python driver")    
+    def test_multi_DCS(self):
+        pass
+
+    @unittest.skip("unsupported by python driver")
+    def test_kerberos_authentication(self):
+        pass
+
+    @unittest.skip("unsupported by python driver")
+    def test_multi_query(self):
+        pass
